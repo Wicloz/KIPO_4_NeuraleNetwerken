a8580317f1ceae7c196e527f0582534da3cf20dd
Wilco de Boer <wfh@wilcodeboer.me>
2018-04-26 13:53:11 +0200

    skelet

---
+++ DeBoerSpaink.cc
@@ -0,0 +1,89 @@
+
+//
+// C++-programma voor neuraal netwerk (NN) met één output-knoop
+// Zie www.liacs.leidenuniv.nl/~kosterswa/AI/nnhelp.pdf
+// 19 april 2018
+// Compileren: g++ -Wall -O2 -o nn nn.cc
+// Gebruik:    ./nn <inputs> <hiddens> <epochs>
+// Voorbeeld:  ./nn 2 3 100000
+//
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+const int MAX = 20;
+const double ALPHA = 0.1;
+const double BETA = 1.0;
+
+// g-functie (sigmoid)
+double g(double x) {
+    return 1 / (1 + exp(-BETA * x));
+}//g
+
+// afgeleide van g
+double gprime(double x) {
+    return BETA * g(x) * (1 - g(x));
+}//gprime
+
+int main(int argc, char* argv[]) {
+
+    int inputs, hiddens;            // aantal invoer- en verborgen knopen
+    double input[MAX];              // de invoer is input[1]...input[inputs]
+    double inputtohidden[MAX][MAX]; // gewichten van invoerknopen 0..inputs
+    // naar verborgen knopen 1..hiddens
+    double hiddentooutput[MAX];     // gewichten van verborgen knopen 0..hiddens
+    // naar de ene uitvoerknoop
+    double inhidden[MAX];           // invoer voor de verborgen knopen 1..hiddens
+    double acthidden[MAX];          // en de uitvoer daarvan
+    double inoutput;                // invoer voor de ene uitvoerknoop
+    double netoutput;               // en de uitvoer daarvan: de net-uitvoer
+    double target;                  // gewenste uitvoer
+    double error;                   // verschil tussen gewenste en
+    // geproduceerde uitvoer
+    double delta;                   // de delta voor de uitvoerknoop
+    double deltahidden[MAX];        // de delta's voor de verborgen
+    // knopen 1..hiddens
+    int epochs;                     // aantal trainingsvoorbeelden
+    int i, j, k;                    // tellertjes
+    //int seed = 1234;                // eventueel voor random-generator
+
+    if (argc != 4) {
+        cout << "Gebruik: " << argv[0] << " <inputs> <hiddens> <epochs>" << endl;
+        return 1;
+    }//if
+    inputs = atoi(argv[1]);
+    hiddens = atoi(argv[2]);
+    epochs = atoi(argv[3]);
+    input[0] = -1;                  // invoer bias-knoop: altijd -1
+    acthidden[0] = -1;              // verborgen bias-knoop: altijd -1
+    srand(time(NULL));
+
+    //TODO-1 initialiseer de gewichten random tussen -1 en 1:
+    // inputtohidden en hiddentooutput
+    // rand ( ) levert geheel randomgetal tussen 0 en RAND_MAX; denk aan casten
+
+    for (i = 0; i < epochs; i++) {
+
+        //TODO-2 lees een voorbeeld in naar input en target, of genereer dat ter plekke:
+        // als voorbeeld: de XOR-functie, waarvoor geldt dat inputs = 2
+        // int x = rand ( ) % 2; int y = rand ( ) % 2; int dexor = ( x + y ) % 2;
+        // input[1] = x; input[2] = y; target = dexor;
+
+        //TODO-3 stuur het voorbeeld door het netwerk
+        // reken inhidden's uit, acthidden's, inoutput en netoutput
+
+        //TODO-4 bereken error, delta, en deltahidden
+
+        //TODO-5 update gewichten hiddentooutput en inputtohidden
+
+    }//for
+
+    //TODO-6 beoordeel het netwerk en rapporteer
+
+    return 0;
+}//main
+
